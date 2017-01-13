6a783b8c8d9b1641e037b6502d9a0e76a4a6b4d3
SebSnares <sibbe007@gmail.com>
2017-01-13 17:09:24 +0100

    changing walltime back for long runs

--- scripts/run_monee.sh
+++ scripts/run_monee.sh
@@ -6,7 +6,7 @@
 #walltime for quicktest 00:04:59
 #walltime for iterations 2.000.0000 00:14:59
 #walltime for iterations 1.000.0000 00:09:59
-#PBS -lnodes=1 -lwalltime=00:04:59
+#PBS -lnodes=1 -lwalltime=00:14:59
 
 ### Set the simulation-specific parameters
 #--------------------------------------------------------------------------------------------------#
