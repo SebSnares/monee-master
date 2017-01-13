f3b80938a3f4de63930eb7dbc987abf27db4e272
SebSnares <sibbe007@gmail.com>
2017-01-13 13:20:22 +0100

    bugfix in gode
    forgot some ; at line ends

--- prj/SimpleShells/src/SimpleShellsControlArchitecture.cpp
+++ prj/SimpleShells/src/SimpleShellsControlArchitecture.cpp
@@ -257,7 +257,7 @@ void SimpleShellsControlArchitecture::assignFitness(std::vector<Genome> & genome
 	puckPrices.resize(gPuckColors);
 	double steep = (double) _sigmarketSlope; //added for control over "k" in logistic function *test run with high value similar to step function in jones&mataric work 2003
 	double desiredRatio = 1.0/(double)gPuckColors; // for even distribution of pucktypes collected - to put more weight on some pucktypes use 'i' in for-loop
-	double currentRatio = 2.0 //set to 2.0 becaue a ratio of 2 is impossible -> to check if current ratio is changed
+	double currentRatio = 2.0; //set to 2.0 becaue a ratio of 2 is impossible -> to check if current ratio is changed
 	std::cout << "desired ratio:" << desiredRatio; //print desiredration (should be 0.5)
 	for (int i = 0; i < gPuckColors; i++) {
 		if (i != _wm->_energyPuckId || !_wm->_excludeEnergyPucks) {
@@ -266,7 +266,7 @@ void SimpleShellsControlArchitecture::assignFitness(std::vector<Genome> & genome
 				std::cout << "use market" << '\n';
                 if (_useSigMarket) {
                    std::cout << "use sigmarket with steep: " << steep << '\n';
-                   currentRatio = double(puckTotals[i])/double(cumTotal)  //calculate current puck ratio per type/total
+                   currentRatio = double(puckTotals[i])/double(cumTotal);  //calculate current puck ratio per type/total
                    std::cout << "puck ratio: " << double(puckTotals[i]) << "/" << double(cumTotal) << " = " << currentRatio '\n';
                    puckPrices[i] = (puckTotals[i] == 0) ? 0.0 : 1.0 / (1.0 + std::exp( steep * (currentRatio - desiredRatio))); // MARKET MECHANISM the exchange reate is defined here note: (puckTotals==0)? 0 : total/puckTotals means if pucktotals is 0 use 0 otherwise use Total/puckTotal -> avoid division by 0
                 } else {
